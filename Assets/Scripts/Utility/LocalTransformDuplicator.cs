fc1a504516b5c9a5dfe1ed8b371c05898096ceb8
Sven van den Tweel <sven.vd.tweel@gmail.com>
2021-08-19 14:45:57 +0200

    Added scripts to copy a template scene with all the prefabs and configuration

--- Assets/Scripts/Utility/LocalTransformDuplicator.cs
+++ Assets/Scripts/Utility/LocalTransformDuplicator.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 //duplicate the given transform position, rotation and scale when this object get enable
 public class LocalTransformDuplicator : MonoBehaviour
