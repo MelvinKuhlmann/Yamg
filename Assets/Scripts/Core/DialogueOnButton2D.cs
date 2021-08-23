1f557d35919effa117098fa4e6b9ec5361d3b5a2
Sven van den Tweel <sven.vd.tweel@gmail.com>
2021-08-23 22:00:41 +0200

    #25 Extra script to have some more options for handling conversations. Changed layer of money to PlayerDrop and updated the physics collision in settings. Added a simple INSPECT overlay to indicate the player can do something..

---
+++ Assets/Scripts/Core/DialogueOnButton2D.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using System.Collections;
+
+public class DialogueOnButton2D : InteractOnTrigger2D
+{
+    public UnityEvent OnButtonPress;
+    public UnityEvent OnButtonPressAfterPreConditions;
+
+    bool m_CanExecuteButtons;
+    bool m_preConditionsMet = false;
+
+    protected override void ExecuteOnEnter(Collider2D other)
+    {
+        m_CanExecuteButtons = true;
+        OnEnter.Invoke();
+    }
+
+    protected override void ExecuteOnExit(Collider2D other)
+    {
+        m_CanExecuteButtons = false;
+        m_preConditionsMet = false;
+        OnExit.Invoke();
+    }
+
+    void Update()
+    {
+        if (m_CanExecuteButtons)
+        {
+            if (OnButtonPress.GetPersistentEventCount() > 0 && PlayerInput.Instance.Interact.Down && !m_preConditionsMet)
+            {
+                OnButtonPress.Invoke();
+                m_preConditionsMet = true;
+            }
+            else if (OnButtonPressAfterPreConditions.GetPersistentEventCount() > 0 && PlayerInput.Instance.Interact.Down && m_preConditionsMet)
+            {
+                OnButtonPressAfterPreConditions.Invoke();
+                // This avoids being retriggered again in the same frame update
+                StartCoroutine(WaitForRetrigger());
+            }
+        }
+    }
+
+    private IEnumerator WaitForRetrigger()
+    {
+        yield return new WaitForSeconds(0.1f);
+        m_preConditionsMet = false;
+    }
+}
