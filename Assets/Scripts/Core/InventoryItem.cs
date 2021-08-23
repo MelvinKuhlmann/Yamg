1ec08f965185b69eefe4a871a1a4238659781511
Sven van den Tweel <sven.vd.tweel@gmail.com>
2021-08-23 20:17:14 +0200

    #24 added script to persist the current state of the money chest/rock

--- Assets/Scripts/Core/InventoryItem.cs
+++ Assets/Scripts/Core/InventoryItem.cs
@@ -1,7 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
+﻿using UnityEngine;
 
 [RequireComponent(typeof(CircleCollider2D))]
 public class InventoryItem : MonoBehaviour, IDataPersister
---
+++ Assets/Scripts/Interactables/MoneyChest.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+public class MoneyChest : MonoBehaviour, IDataPersister
+{
+    public GameObject incompletedState;
+    public GameObject completedState;
+    public DataSettings dataSettings;
+
+    void OnEnable()
+    {
+        PersistentDataManager.RegisterPersister(this);
+    }
+
+    void OnDisable()
+    {
+        PersistentDataManager.UnregisterPersister(this);
+    }
+
+    public void Save()
+    {
+        PersistentDataManager.SetDirty(this);
+    }
+
+    public DataSettings GetDataSettings()
+    {
+        return dataSettings;
+    }
+
+    public void SetDataSettings(string dataTag, DataSettings.PersistenceType persistenceType)
+    {
+        dataSettings.dataTag = dataTag;
+        dataSettings.persistenceType = persistenceType;
+    }
+
+    public Data SaveData()
+    {
+        bool isCompleted = !incompletedState.activeInHierarchy && completedState.activeInHierarchy;
+        return new Data<bool>(isCompleted);
+    }
+
+    public void LoadData(Data data)
+    {
+        Data<bool> isCompleted = (Data<bool>)data;
+        incompletedState.SetActive(!isCompleted.value);
+        completedState.SetActive(isCompleted.value);
+    }
+}
