e2e1e30309f06928311f34d0ee05dae5b35ee3af
Sven van den Tweel <sven.vd.tweel@gmail.com>
2021-08-25 21:29:31 +0200

    #37 trigger visual effect only after healing is completed

--- Assets/Scripts/Character/StateMachineBehaviours/Player/HealingSMB.cs
+++ Assets/Scripts/Character/StateMachineBehaviours/Player/HealingSMB.cs
@@ -4,6 +4,9 @@ public class HealingSMB : SceneLinkedSMB<PlayerCharacter>
 {
     private float timer;
     int m_HashLocomotionState = Animator.StringToHash("Locomotion");
+    private string vfxName = "HealingEffect";
+    private Vector3 offset = Vector3.zero;
+    private float startDelay = 0;
 
     public override void OnSLStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
@@ -15,8 +18,9 @@ public class HealingSMB : SceneLinkedSMB<PlayerCharacter>
         m_MonoBehaviour.CheckForHealing();
         if (Time.time - timer > m_MonoBehaviour.healingDuration)
         {
-            Debug.LogError("Healing Done");
             m_MonoBehaviour.Heal();
+            VFXController.Instance.Trigger(vfxName, offset, startDelay, false, animator.transform);
+            // To prevent all missing health will be added at once we explicitly set the state to locomotion, so if you keep down healing, it will trigger the proces all over again.
             animator.Play(m_HashLocomotionState, layerIndex, stateInfo.normalizedTime);
         }
     }
